202ada1c6e1fbca058379109a5721e54bda43cab
Stoyan Valchev <stoyan.v.valchev@gmail.com>
2022-12-12 18:36:28 +0200

    test 1

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-
+  var productDiv = document.querySelector(".product");
+  var productPrice = document.querySelectorAll("p")[1].innerHTML;
+  var span = document.createElement("span");
+  span.class = "data-price";
+  span.className = "data-price";
+  span.innerText = productPrice;
+  productDiv.setAttribute("data-price", productPrice);
+  productDiv.appendChild(span);
 });
