1fc9874bc3979217230f5d7e9e705789a8251ce6
Stoyan Valchev <stoyan.v.valchev@gmail.com>
2022-12-22 12:37:14 +0200

    test2

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-
+  let priceValue = document.querySelector(".price").textContent;
+  let product = document.querySelector(".product");
+  product.setAttribute("data-price", priceValue);
 });
